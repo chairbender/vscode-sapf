57cfe4516a4e9b97461b43c70a8c817a214f91ab
chairbender <kwhipke1@gmail.com>
2025-03-16 13:42:43 -0700

    WIP implementing actual commands

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # SAPF Extension for VS Code
 
-## Features
+WIP. Contributions welcome. I'm a typescript novice.
 
-1. Connect to the sapf-lsp for highlighting, autocompletion, etc... see https://github.com/vasilymilovidov/sapf-lsp for what features are supported (they should work mostly
-"automatically" in VS Code).
-2. Interact with sapf (evaluate code, start, stop, etc...)
-
+This relies on https://github.com/vasilymilovidov/sapf-lsp for the LSP.
 
 ## Setup
 
 1. Follow insructions at https://github.com/vasilymilovidov/sapf-lsp to build the LSP
 1. Make sure LSP is on your PATH (if you use `cargo install --path .` to build the above repo, I think that should work).
 1. Make sure the sapf executable is also on your PATH.
-1. If your PATH changed, you may need to restart VSCode to pick up the change.
+1. If you had to change your PATH variable, you may need to restart VSCode to pick up the change.
 1. Extension will apply when file ends in .sapf or can be chosen manually via VSCode (bottom right).
+1. Bring up the command palette and type "sapf" to see available commands.
+
+## Features
+
+1. Connect to the sapf-lsp for highlighting, autocompletion, etc... see https://github.com/vasilymilovidov/sapf-lsp for what features are supported (they should work mostly
+"automatically" in VS Code).
+2. Interact with sapf (evaluate code, start, stop, etc...)
--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,13 @@
 import * as vscode from 'vscode';
-import * as path from 'path';
+import { spawn, ChildProcessWithoutNullStreams } from "child_process";
 import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient/node';
 
 let client: LanguageClient;
+let replProcess: ChildProcessWithoutNullStreams | null = null;
+let outputChannel = vscode.window.createOutputChannel("sapf");
 
 export function activate(context: vscode.ExtensionContext) {
+    // TODO: we need to log what's happening to a special output
     let serverCommand = 'sapf-lsp';
     let serverOptions: ServerOptions = {
         run: { command: serverCommand, options: { env: { PATH: process.env.path }}, transport: TransportKind.stdio },
@@ -15,8 +18,86 @@ export function activate(context: vscode.ExtensionContext) {
         documentSelector: [{ scheme: 'file', language: 'sapf' }]
     };
 
+    // TODO: auto switch to output channel?
+
     client = new LanguageClient('SapfLanguageServer', 'sapf Language Server', serverOptions, clientOptions);
+    outputChannel.show();
+
+    outputChannel.appendLine("Starting sapf language server...")
     client.start();
+    outputChannel.appendLine("Started sapf language server.")
+
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand("sapf.start", startRepl), 
+        vscode.commands.registerCommand("sapf.kill", killRepl),
+        vscode.commands.registerCommand("sapf.stop", sapfStop),
+        vscode.commands.registerCommand("sapf.clear", sapfClear),
+        vscode.commands.registerCommand("sapf.cleard", sapfCleard),
+        // evals block or current selection
+        vscode.commands.registerCommand("sapf.evalBlock", evalBlock),
+        // eval line or current selection
+        vscode.commands.registerCommand("sapf.evalLine", evalLine),
+        vscode.commands.registerCommand("sapf.helpWord", helpWord),
+        vscode.commands.registerCommand("sapf.helpAll", helpAll)
+    );
+
+}
+
+function startRepl() {
+    if (replProcess) {
+        vscode.window.showInformationMessage("sapf is already running.");
+        return;
+    }
+
+    replProcess = spawn("sapf", [], { env: { PATH: process.env.PATH }, shell: true });
+
+    replProcess.stdout.on("data", (data) => outputChannel.append(data.toString()));
+    replProcess.stderr.on("data", (data) => outputChannel.append(data.toString()));
+
+    replProcess.on("exit", () => {
+        vscode.window.showInformationMessage("sapf exited.");
+        replProcess = null;
+    });
+
+    outputChannel.show();
+}
+
+function killRepl() {
+    if (!replProcess) {
+        vscode.window.showInformationMessage("sapf is not running.");
+        return;
+    }
+
+    outputChannel.appendLine("Stopping sapf...")
+    if (replProcess.kill()) {
+        outputChannel.appendLine("sapf stopped.")
+    } else {
+        outputChannel.appendLine("Unable to stop sapf.")
+    }
+
+    outputChannel.show();
+}
+
+function sendToRepl() {
+    if (!replProcess) {
+        vscode.window.showErrorMessage("sapf is not running.");
+        return;
+    }
+
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        vscode.window.showErrorMessage("No active editor.");
+        return;
+    }
+
+    const selectedText = editor.document.getText(editor.selection);
+    if (!selectedText) {
+        vscode.window.showErrorMessage("No text selected.");
+        return;
+    }
+
+    replProcess.stdin.write(selectedText + "\n");
 }
 
 export function deactivate(): Thenable<void> | undefined {
